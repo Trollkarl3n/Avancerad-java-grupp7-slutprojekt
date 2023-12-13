321f4a47966001d0662c8ac3488030eb08c74b08
Erik Werther <erik.werther@gmail.com>
2023-12-13 11:09:07 +0100

    gjort till private

--- src/main/java/com/DiscordBot/Main.java
+++ src/main/java/com/DiscordBot/Main.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
     public static void main (String[] args){
-        JDA jda = JDABuilder.createDefault("").build();
+        JDA jda = JDABuilder.createDefault("MTE4NDQyMDY2MjQ3NjAzNDA0OA.GQAPVl.uvILNGUzO8QphbBED2z_tKuppWe1hd3D2zjIfk").build();
     }
 }
 

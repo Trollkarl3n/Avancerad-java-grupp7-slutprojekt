59e8119a40c119fd5e8721d09442a51d5e70049b
Erik Werther <erik.werther@gmail.com>
2023-12-13 10:18:16 +0100

    fixa build samt tokens

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,12 @@ plugins {
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation("net.dv8tion:JDA:5.0.0-beta.18")
 }
---
+++ src/main/java/com/DiscordBot/Main.java
@@ -0,0 +1,11 @@
+package com.DiscordBot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+    public static void main (String[] args){
+        JDA jda = JDABuilder.createDefault("MTE4NDQyMDY2MjQ3NjAzNDA0OA.GNA0qE.4CAx9G97uowQcVlzPWOokQi4q1WKD_6V_g41jg").build();
+    }
+}
+

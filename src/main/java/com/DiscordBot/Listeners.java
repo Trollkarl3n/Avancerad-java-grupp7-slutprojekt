2a9e079b88475177caaf7731f03baf2889a38688
NeilElvirsson <151514156+NeilElvirsson@users.noreply.github.com>
2023-12-14 10:03:04 +0100

    Trycatch

--- src/main/java/com/DiscordBot/Listeners.java
+++ src/main/java/com/DiscordBot/Listeners.java
@@ -1,6 +1,9 @@
 package com.DiscordBot;
 
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -15,12 +18,16 @@ public class Listeners extends ListenerAdapter {
         guild.upsertCommand("sum", "Gives the sum of two numbers").addOptions(new OptionData(OptionType.INTEGER, "number1", "The first number", true).setMinValue(1).setMaxValue(100)).queue();
     }
 
-    /*
+
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        if(event.getAuthor().isBot()) return;
-        MessageChannelUnion channel = event.getChannel();
-        channel.sendMessage(event.getMessage().getContentRaw()).queue();
+        try {
+            if (event.getAuthor().isBot()) return;
+            MessageChannelUnion channel = event.getChannel();
+            channel.sendMessage(event.getMessage().getContentRaw()).queue();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-     */
+
 }
